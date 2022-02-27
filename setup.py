8e952b62318f95f58a59f965c6c084f0649d80fc
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2022-02-27 02:39:26 +0000

    Update setup.py and switch to setuptools instead of deprecated distutils.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #+
-# Distutils script to install qahirah_xcffib. Invoke from the command line
-# in this directory as follows:
+# Setuptools script to install DBussy. Make sure setuptools
+# <https://setuptools.pypa.io/en/latest/index.html> is installed.
+# Invoke from the command line in this directory as follows:
 #
 #     python3 setup.py build
 #     sudo python3 setup.py install
@@ -9,39 +10,41 @@
 #-
 
 import sys
-import distutils.core
-from distutils.command.build import \
-    build as std_build
+import setuptools
+from setuptools.command.build_py import \
+    build_py as std_build_py
 
-class my_build(std_build) :
+class my_build_py(std_build_py) :
     "customization of build to perform additional validation."
 
     def run(self) :
         try :
-            class dummy :
-                pass
-            #end dummy
-            dummy.__doc__ = "something"
-        except AttributeError :
-            sys.stderr.write("This module requires Python 3.3 or later.\n")
+            exec \
+              (
+                "async def dummy() :\n"
+                "    pass\n"
+                "#end dummy\n"
+              )
+        except SyntaxError :
+            sys.stderr.write("This module requires Python 3.5 or later.\n")
             sys.exit(-1)
         #end try
         super().run()
     #end run
 
-#end my_build
+#end my_build_py
 
-distutils.core.setup \
+setuptools.setup \
   (
     name = "Qahirah_xcffib",
-    version = "0.92",
-    description = "language bindings for the Cairo graphics library, for Python 3.3 or later",
+    version = "0.1",
+    description = "asyncio-friendly language bindings for xcffib, for Python 3.5 or later",
     author = "Lawrence D'Oliveiro",
     author_email = "ldo@geek-central.gen.nz",
     url = "https://github.com/ldo/qahirah_xcffib",
     py_modules = ["qahirah_xcffib"],
     cmdclass =
         {
-            "build" : my_build,
+            "build_py" : my_build_py,
         },
   )
